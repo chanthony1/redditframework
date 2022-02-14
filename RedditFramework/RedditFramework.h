c4b36ca82dcc911de065e169afec14af5baac43e
cquicano <ceo@christianquicano.pro>
2022-02-14 09:59:11 -0500

    first commit

---
+++ RedditFramework/RedditFramework.h
@@ -0,0 +1,18 @@
+//
+//  RedditFramework.h
+//  RedditFramework
+//
+//  Created by Christian Quicano on 2/14/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RedditFramework.
+FOUNDATION_EXPORT double RedditFrameworkVersionNumber;
+
+//! Project version string for RedditFramework.
+FOUNDATION_EXPORT const unsigned char RedditFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedditFramework/PublicHeader.h>
+
+
